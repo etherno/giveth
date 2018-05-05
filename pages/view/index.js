a77f8a27075c2d81dffadf2112a78aa84d08f624
deam <deamkh@gmail.com>
2018-05-05 09:25:28 +0200

    Project update

--- pages/view/index.js
+++ pages/view/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 import firebase from "firebase";
+import styled from 'styled-components';
 import { withI18next } from "../../lib/withI18next";
 import initFirebase from "../../lib/initFirebase";
 
+
+import { Box } from 'grid-styled';
 import MainMenu from "../../components/MainMenu";
 
 initFirebase();
+
+const Container = styled.div`
+  margin 0 auto;
+  max-width 48rem;
+  margin-top: 6rem;
+  text-align: center;
+`
+
+const Video = styled.video`
+  max-width 48rem;
+`
+
+const Title = styled.h1`
+  font-family: Quicksand;
+  color: #2c0d54;
+`
 
 class View extends Component {
   static async getInitialProps({ query }) {
@@ -37,7 +56,10 @@ class View extends Component {
     return (
       <div>
         <MainMenu />
-        <video src={media.src} />
+        <Container>
+          <Title>{media && media.title}</Title>
+          <Video src={media && media.src} loop controls autoPlay />
+        </Container>
       </div>
     );
   }
