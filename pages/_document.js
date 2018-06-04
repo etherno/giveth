95054bdf289f1e6b18113e7cc590726131d755f2
deam <deamkh@gmail.com>
2018-06-04 22:11:07 +0200

    Project update

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,6 @@ export default class MyDocument extends Document {
   render () {
     return (
       <html>
-        <html lang="en" />
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" content="Charitable giving on the blockchain - revolution.eth" />
